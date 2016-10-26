fedbdfbe6a0f918a7504e193b3aa9599cfde6736
Jacob Ernst <Jacobaernst@gmail.com>
2016-10-26 14:14:59 -0400

    Add files via upload

---
+++ ErnstProject5/Form1.vb
@@ -0,0 +1,29 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'error check
+        If Not IsNumeric(TextBox1.Text) Then
+            TextBox1.Clear()
+            MsgBox("Please only enter numbers")
+            Exit Sub
+        End If
+        If Val(TextBox1.Text) <= 0 Then
+            TextBox1.Clear()
+            MsgBox("Please enter positave values")
+            Exit Sub
+        End If
+        If ListBox1.Items.Count = 10 Then
+            TextBox1.Clear()
+            MsgBox("No more bills can be entered")
+        End If
+        ListBox1.Items.Add(TextBox1.Text)
+        TextBox1.Clear()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If ListBox1.SelectedIndex = -1 Then
+            MsgBox("Please select a grade to remove")
+            Exit Sub
+        End If
+        ListBox1.Items.Remove(ListBox1.SelectedItem)
+    End Sub
+End Class
---
+++ ErnstProject5/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("ErnstProject5")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("ErnstProject5")>
+<Assembly: AssemblyCopyright("Copyright ©  2016")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("4de8bb7f-dd7b-489a-be01-ce1b51098269")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
